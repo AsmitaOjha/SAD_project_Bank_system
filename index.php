1c58ae36982df5522242564cf6531eeac2b4d6f9
Asmita Ojha <asmitaojha05@gmail.com>
2023-11-07 17:23:09 +0545

    23rd day BANK SYSTEM

--- index.php
+++ index.php
@@ -12,9 +12,13 @@
         /* Style for the navigation bar */
         .navbar {
             background-color: #333;
-            overflow: hidden;
+            /* overflow: hidden; */
             display:flex;
             justify-content: center;
+            /* position: fixed; Add position: fixed to make the navigation bar fixed */
+            width: 100%; /* Make the navigation bar span the full width of the viewport */
+            top: 0; /* Position it at the top of the viewport */
+            z-index: 1000; /* Ensure it's above other elements */
         }
 
         /* Style for the navigation bar links */
@@ -32,9 +36,11 @@
             background-color: #555;
         }
         body{
+            
             font-family:Arial, sans-serif;
             margin:0;
             padding:0;
+            overflow-y: auto;
         }
         .content-container{
             background:linear-gradient(to bottom, rgba(66, 67, 60, 0.2), rgba(55, 55, 55, 0.8)),url('cash.jpg') no-repeat center fixed;
@@ -45,6 +51,7 @@
             justify-content:center;
              /* align-items:center;  */
             color: #000;
+            display:flex;
         }
        
 
@@ -60,8 +67,20 @@
         <a href="withdraw.php">Withdraw</a>
     </div>
     <div class='content-container'>
+    <div class="container1">
     <h1>Welcome to RAW Bank</h1>
-   
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempore nemo fugit.</p>
+    <span>Find out more</span>
+    </div>
+    <div class="container2">
+    <form>
+        <input type="text" placeholder="your name">
+        <input type="text" placeholder="your email">
+        <input type="text" placeholder="your phone">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione non quis omnis quos mollitia delectus tempore repudiandae recusandae illum. Fuga mollitia est consectetur illo error. Voluptatem hic dolor delectus sed.
+        <button>Submit</button>
+    </form>
+    </div>
     </div>
     <!-- Your page content goes here -->
 
